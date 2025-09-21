895c1c48495f40dc4cabe168a5083fad4b0d8c54
shivanshutiwari <shivanshu.tiwari.home@gmail.com>
2025-09-21 14:31:15 +0530

    Tic-Tac-toe: adding winning strategy

--- src/Tic_tak_toe/Strategy/RowWinningStrategy.java
+++ src/Tic_tak_toe/Strategy/RowWinningStrategy.java
@@ -3,10 +3,25 @@ package Tic_tak_toe.Strategy;
 import Tic_tak_toe.Model.Board;
 import Tic_tak_toe.Model.Move;
 
+import java.util.HashMap;
+
 public class RowWinningStrategy implements WinningStrategy{
 
+    HashMap<Integer, HashMap<String,Integer>> rowWinning = new HashMap<>();
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+        int row = move.getCell().getRow();
+        String symbol = move.getPlayer().getSymbol().getName();
+        if(!rowWinning.containsKey(row)){
+            rowWinning.put(row,new HashMap<>());
+
+        }else if(!rowWinning.get(row).containsKey(symbol)){
+            rowWinning.get(row).put(symbol,1);
+
+        }
+        rowWinning.get(row).put(symbol,rowWinning.get(row).get(symbol)+1);
+
+        return   rowWinning.get(row).get(symbol) == board.getSize();
     }
+
 }
